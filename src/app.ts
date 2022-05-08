a26ae7c7f77eb003fe2c8b83238adef2a38dd260
c0dejohn <jemanyoma07@gmail.com>
2022-05-08 17:24:50 -0500

    updates

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,23 @@
-import express from 'express'
-const app = express()
+import * as express from "express";
+// import productRouter from "./router/productos.routes";
+// import cartRouter from "./router/carrito.routes";
+import config from "./config";
+import { createConnection } from "typeorm";
+const app = express();
 
-app.use(express)
+createConnection(config.typeOrmConfig)
+
+// import { logger } from "./util/logger";
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+    res.json({ status: "ok" })
+})
+
+const { PORT } = config;
+
+app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`);
+});
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,9 @@ const cartRouter = Router();
 
 // const carrito = new Carrito();
 
-cartRouter.get("/:id/productos", carrito.getById);
-cartRouter.post("/", carrito.addProduct);
-cartRouter.put(`/:id`, carrito.update);
-cartRouter.delete(`/:id`, carrito.destroy);
+// cartRouter.get("/:id/productos", carrito.getById);
+// cartRouter.post("/", carrito.addProduct);
+// cartRouter.put(`/:id`, carrito.update);
+// cartRouter.delete(`/:id`, carrito.destroy);
 
 export default cartRouter;
